9e7425f9ebcece577c82bb7a2595299bc68bc76d
cyril <cyril@dkeystone.com>
2015-03-04 15:55:18 -0800

    photo

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ passport.use('local', new LocalStrategy({
 passport.use(new FacebookStrategy({
     clientID: "346135978909744",
     clientSecret: "38d4304faea16f2ec4747d39e8ed2422",
-    callbackURL: "http://places-1.herokuapp.com/facebook",
+    callbackURL: (process.env.DOMAIN || config.local.DOMAIN) + "/facebook",
     profileFields: ['id', 'displayName','picture.type(large)', 'emails']
   },
   function(accessToken, refreshToken, profile, done) {
